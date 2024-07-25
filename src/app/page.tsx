bb5d3cbda82176730877b1d0898b2dd19e2151b7
piyush <piyushbhatt053@gmail.com>
2024-07-25 13:42:18 +0530

    new update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import FeaturedCources from "@/components/FeaturedCources";
 import HeroSection from "@/components/HeroSection";
+import TestimonialCards from "@/components/TestimonialCards";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 export default function Home() {
@@ -7,6 +9,8 @@ export default function Home() {
    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02">
     <HeroSection/>
     <FeaturedCources/>
+    <WhyChooseUs/>
+    <TestimonialCards/>
    </main>
   );
 }
